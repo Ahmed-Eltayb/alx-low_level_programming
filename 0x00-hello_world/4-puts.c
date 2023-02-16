1aec3a7b222266e39b4ff129933b0b2bf87d53a1
Ahmed-Eltayb <eltayb_44@yahoo.com>
2023-02-16 15:40:16 -0800

    first C program using put()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,8 @@
 
 int main(void)
 	{
-		char my_string[] = "\"Programming is like building a multilingual puzzle" 
+		puts("\"Programming is like building a multilingual puzzle"); 
 
-		puts(my_string);
 
 		return(0);
 	}	
