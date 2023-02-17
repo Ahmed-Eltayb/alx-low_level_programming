8b9b7e0159fe632b8bd33fd7311775349684750c
Ahmed-Eltayb <eltayb_44@yahoo.com>
2023-02-16 16:57:37 -0800

    C program to print a quote using putchar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1
  */
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
     return (1);
 }
