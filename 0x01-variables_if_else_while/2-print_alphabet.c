d975131d619f4b182971f8891a1a7acbb24f0271
Ahmed-Eltayb <eltayb_44@yahoo.com>
2023-02-17 08:52:08 -0800

    C program to output a to z

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	char a = "a";
+	char a = 'a';
 
-	while (a <= "z")
+	while (a <= 'z')
 	{
 		putchar(a);
 		a++;
 	}
-	putchar("\n");
+	putchar('\n');
 
     return (0);
 }
