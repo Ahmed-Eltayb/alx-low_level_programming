a90942c3ff4d9cf9dd07afce23d94f5f43bb9d19
Ahmed-Eltayb <eltayb_44@yahoo.com>
2023-02-21 16:10:50 -0800

    Print the alphbet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - prints from a to z x10
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+	int b = 0;
+	while (b <= 9)
+	{
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		b++;
+	}
+	_putchar('\n');
+}
