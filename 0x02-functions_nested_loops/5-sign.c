6854c0668d78c0157a767633c395724cba52929f
Ahmed-Eltayb <eltayb_44@yahoo.com>
2023-02-21 16:51:44 -0800

    check the sign of the number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include"main.h"
 /**
- * print_sign - Returns 1 and prints + if n is greater than zero Returns 0 and prints 0 if n is zero Returns -1 and prints - if n is less than zero
+ * print_sign - Returns 1 if + , 0 if 0 , -1 if -
  *
  * @n: input number
  */
@@ -18,7 +18,7 @@ int print_sign(int n)
 			_putchar('-');
 			return (-1);
 		}
-		else 
+		else
 		{
 			_putchar('0');
 			return (0);
