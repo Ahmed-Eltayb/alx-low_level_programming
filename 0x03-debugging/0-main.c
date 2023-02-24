313bd7c18e9adca6c45980f6cb9593f52cfd9b20
Ahmed-Eltayb <eltayb_44@yahoo.com>
2023-02-23 16:48:00 -0800

    Debug for positive or negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * main - debugging positive or negative function
+ *
+ * @n: input number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	positive_or_negative(n);
+
+	return (0);
+}
