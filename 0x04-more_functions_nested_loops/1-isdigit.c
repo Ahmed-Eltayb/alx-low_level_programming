92a683d48a4a4af318613645226fcb54be9af1f7
Ahmed-Eltayb <eltayb_44@yahoo.com>
2023-02-23 17:35:35 -0800

    c program to check for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+
+/**
+ * _isdigit - checks for digits
+ *
+ * @c: input
+ *
+ * Return: 1 if digit, 0 if otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
