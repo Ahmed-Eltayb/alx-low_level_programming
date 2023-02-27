84dd96e8bace09630006ca5305ad36fcf11d3b81
Ahmed-Eltayb <eltayb_44@yahoo.com>
2023-02-27 09:00:57 -0800

    function that reset input to 98 using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * reset_to_98 - function that takes a pointer
+ * and updates the value it points to to 98.
+ *
+ * @n: input number
+ * @*p: pointer that have the address of n
+ */
+void reset_to_98(int *n)
+{
+	int n;
+	int *p = &n;
+
+	*p = 98;
+	return (0);
+}
