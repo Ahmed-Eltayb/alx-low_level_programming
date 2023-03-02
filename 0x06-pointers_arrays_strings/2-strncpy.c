c982478a2001e6055455d53dfae473c99fe9c966
Ahmed-Eltayb <eltayb_44@yahoo.com>
2023-03-02 10:52:39 -0800

    function that concatenates two strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int a;
 
-	for (a = 0; src[a] != '\0'; a++)
+	for (a = 0; a < n && src[a] != '\0'; a++)
 	{
-		if (a < n)
-		{
-			dest[a] = src[a];
-		}
-		while (a < n)
-		{
-			dest[a++] = '\0';
-		}
+		dest[a] = src[a];
 	}
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
 	return (dest);
 }
