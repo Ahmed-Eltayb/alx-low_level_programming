354a7e9c3a54fda5c60b15d8a632f6173473e912
Ahmed-Eltayb <eltayb_44@yahoo.com>
2023-03-10 14:20:57 -0800

    shows the number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: contains the number of arg
+ * @argv: the array that contains the arg of the command line
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
