616be580fe216e627ea5cbecbc868e44a0136241
Ahmed-Eltayb <eltayb_44@yahoo.com>
2023-03-10 14:13:02 -0800

    what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: contains the number of arg
+ * @argv: the array that contains the arg of the command line
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
+
